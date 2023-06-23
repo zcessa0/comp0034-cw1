05a3e95284c19d85175803f7442c2e3c868a9d14
Sarah Sanders <sarah.sanders@ucl.ac.uk>
2023-06-23 09:51:51 +0100

    Update test_employee.py

--- coursework2/test_employee.py
+++ coursework2/test_employee.py
@@ -1,34 +1,28 @@
- This is deliberately badly written and does not conform to good practice for test case naming standards
+# This is deliberately badly written and does not conform to good practice for test case naming standards
 import pytest
 
 from employee import Employee
 
 
-@pytest.fixture
-def emp():
+def testemp1():
     e = Employee(name="A N Other", title="Manager", employee_id="12345", salary=45000)
-    return e
-
-
-def testemp1(emp):
     assert emp.department == "HR"
 
 
-def testemp2(emp):
-    result = emp.calculate_monthly_salary(hours_worked=155)
+def testemp2():
+    e = Employee(name="A N Other", title="Manager", employee_id="12345", salary=45000)
+    result = e.calculate_monthly_salary(hours_worked=155)
     assert result == 3875
 
 
-def testemp3(emp):
-    emp.salary = 9000
-    assert emp.salary == 9000
+def testemp3():
+    e = Employee(name="A N Other", title="Manager", employee_id="12345", salary=45000)
+    e.salary = 9000
+    assert e.salary == 9000
 
 
-def testemp4(emp):
-    emp.salary = 19575.67
-    assert emp.salary == 19575.67
+def testemp4():
+    e = Employee(name="A N Other", title="Manager", employee_id="12345", salary=45000)
+    e.salary = 19575.67
+    assert e.salary == 19575.67
 
-
-def testemp5(emp):
-    with pytest.raises(ValueError) as e:
-        emp.salary = 14999

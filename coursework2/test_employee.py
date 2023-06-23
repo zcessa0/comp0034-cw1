af9ffccb0ea3f7d100e9a41edba544807ae7b9a2
Sarah Sanders <sarah.sanders@ucl.ac.uk>
2023-06-23 08:44:55 +0100

    Update test_employee.py

--- coursework2/test_employee.py
+++ coursework2/test_employee.py
@@ -1,8 +1,12 @@
-# Deliberately badly written and with inappropriate test case names
+ This is deliberately badly written and does not conform to good practice for test case naming standards
+import pytest
+
+from employee import Employee
+
 
 @pytest.fixture
 def emp():
-    e = Employee(name="A N Other", title="Manager", employee_id="12345")
+    e = Employee(name="A N Other", title="Manager", employee_id="12345", salary=45000)
     return e
 
 
@@ -11,5 +15,20 @@ def testemp1(emp):
 
 
 def testemp2(emp):
-    emp.calculate_salary(250)
-    assert emp.salary == 250
+    result = emp.calculate_monthly_salary(hours_worked=155)
+    assert result == 3875
+
+
+def testemp3(emp):
+    emp.salary = 9000
+    assert emp.salary == 9000
+
+
+def testemp4(emp):
+    emp.salary = 19575.67
+    assert emp.salary == 19575.67
+
+
+def testemp5(emp):
+    with pytest.raises(ValueError) as e:
+        emp.salary = 14999

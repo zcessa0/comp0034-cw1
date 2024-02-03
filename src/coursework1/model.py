8199e31f0b4a470527dbb9896b4fa19fd743eb95
Verzon <zcessa0@ucl.ac.uk>
2024-02-03 13:45:55 +0000

    fixed runtime error, created database and added data to database

--- src/coursework1/model.py
+++ src/coursework1/model.py
@@ -2,7 +2,7 @@
 from typing import List
 from sqlalchemy import Integer, String, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from src import db
+from src.coursework1 import db
 
 
 class User(db.Model):

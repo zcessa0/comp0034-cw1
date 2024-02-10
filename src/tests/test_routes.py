32246cb8e459290e6acd70ffd6d3aa8615a8b72c
Verzon <zcessa0@ucl.ac.uk>
2024-02-10 14:41:01 +0000

    Add new patch test

--- src/tests/test_routes.py
+++ src/tests/test_routes.py
@@ -82,11 +82,65 @@ def test_get_specific_dataset_2019(client):
     assert response.status_code == 200
     assert response.json == brent_json
 
-    # def test_get_dataset_2019_not_exists(client):
-    #     """
-    #     GIVEN a flask test client 
-    #     WHEN a request is made for a dataset that does not exist
-    #     THEN the response status code should be 404 Not Found
-    #     """
-    #     response = client.get('/dataset_2019/100')
-    #     assert response.status_code == 404
+def test_get_dataset_2019_not_exists(client):
+        """
+        GIVEN a flask test client 
+        WHEN a request is made for a dataset that does not exist
+        THEN the response status code should be 404 Not Found
+        """
+        response = client.get('/dataset_2019/100')
+        assert response.status_code == 404
+
+
+def test_post_dataset_2019(client):
+    """
+    GIVEN a Flask test client
+    AND a valid JSON for a new dataset
+    WHEN a POST request is made to /dataset_2019
+    THEN a new dataset should be added to the database
+    AND the response status code should be 201
+    """
+    # JSON to create a new dataset
+    dataset_json = {
+        "id": 46,
+        "location": "Test",
+        "ps_eligible_2019": 100,
+        "ps_enroll_2019": 345,
+        "sc_eligible_2019": 6780,
+        "sc_enroll_2019": 8990
+    }
+    # Pass the JSON in the HTTP POST request
+    response = client.post('/dataset_2019', json=dataset_json, content_type='application/json')
+    # 201 is the status code for a successful POST request
+    assert response.status_code == 201
+
+def test_dataset_post_error(client):
+    """
+    GIVEN a Flask test client
+    AND JSON for a new dataset that is missing a required field ("dataset")
+    WHEN a post request is made to /dataset_2019
+    THEN the response status code should be 400 Bad Request
+    """
+    missing_dataset_json = {
+        "ps_eligible_2019": 100,
+        "ps_enroll_2019": 1000,
+        "sc_eligible_2019": 100,
+        "sc_enroll_2019": 1000
+    }
+    response = client.post('/dataset_2019', json=missing_dataset_json)
+    assert response.status_code == 400
+
+def test_patch_dataset_2019(client, new_dataset):
+    """
+    GIVEN an existing dataset
+    AND a Flask test client
+    WHEN an UPDATE request is made to /dataset_2019/45 with a new location
+    THEN the response status code should be 200
+    AND the response should include the message 'Dataset updated'
+    """
+    new_dataset_info = {'ps_enroll_2019': 1500}
+    dataset_id = new_dataset['id']
+    response = client.patch(f'/dataset_2019/{dataset_id}', json=new_dataset_info)
+
+    assert response.json['message'] == f'Dataset {dataset_id} updated'
+    assert response.status_code == 200
